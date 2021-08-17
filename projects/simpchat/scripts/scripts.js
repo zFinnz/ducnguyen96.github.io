6481392a3b0ab10d96186df8a0d323f47de03d79
ducnguyen96 <ducnguyen96@users.noreply.github.com>
2021-08-17 15:54:16 +0000

    deploy: 2d28487eabb3742516e06481ac98cdcc8a3e7142

--- projects/simpchat/scripts/scripts.js
+++ projects/simpchat/scripts/scripts.js
@@ -1,14 +1,11 @@
 let socket;
 const connectToNameSpace = (ns) => {
-  socket = io(
-    `https://ducnguyen96.xyz/websocket/:${ns === "tech" ? 3003 : 3002}/${ns}`,
-    {
-      transports: ["websocket"],
-      query: {
-        token: `${document.cookie.split("=")[1]}`,
-      },
-    }
-  );
+  socket = io(`https://ducnguyen96.xyz:${ns === "tech" ? 3003 : 3002}/${ns}`, {
+    transports: ["websocket"],
+    query: {
+      token: `${document.cookie.split("=")[1]}`,
+    },
+  });
 
   socket.on("connect", function () {
     console.log(`connected to ${ns}`);
