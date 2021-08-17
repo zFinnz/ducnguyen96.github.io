2f93e6e38186ff32134f7d45bffd14ca0fdca07e
ducnguyen96 <levinguyen.dl@gmail.com>
2021-08-17 23:05:48 +0700

    :rocket: handshake ws

--- content/projects/simpchat/scripts/scripts.js
+++ content/projects/simpchat/scripts/scripts.js
@@ -1,6 +1,3 @@
-let socket = io(`https://ducnguyen96.xyz:3001`, {
-  transports: ["websocket"],
-});
 const connectToNameSpace = (ns) => {
   socket = io(`https://ducnguyen96.xyz:${ns === "tech" ? 3003 : 3002}/${ns}`, {
     transports: ["websocket"],
